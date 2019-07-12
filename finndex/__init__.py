0ed216a645867be8747727b22f8e04ac66cc260c
Finn Frankis <finn@teachmy.com>
2019-07-12 12:27:27 -0700

    Implement fundamental analysis into library

--- finndex/__init__.py
+++ finndex/__init__.py
@@ -1,3 +1,6 @@
+from pandas.plotting import register_matplotlib_converters
+
 import finndex.sentiment.nlp as nlp
 
 nlp.startServer(nlp.STANFORD_NLP_TIMEOUT, nlp.STANFORD_NLP_PORT)
+register_matplotlib_converters() # register MatPlotLib date converter
