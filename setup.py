d81ecfaf276f479f1a650bb793b7de6b2e6997f9
Finn Frankis <finn@teachmy.com>
2019-07-12 00:37:15 -0700

    Setup basic package structure for publishing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='finndex',
+      version='0.1',
+      description='A useful tool for crypto analysts providing several metrics on various cryptocurrencies.',
+      url='https://github.com/FinnitoProductions/Crypto-Sentiment-Tracker',
+      author='Finn Frankis',
+      author_email='finn@teachmy.com',
+      license='MIT',
+      packages=['finndex'],
+      zip_safe=False)
