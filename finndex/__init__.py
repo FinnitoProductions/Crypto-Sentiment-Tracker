78419f315374d269fd583bd325fa61b7008a6c35
Finn Frankis <finn@teachmy.com>
2019-07-22 23:24:10 -0700

    Add basic API for data retrieval using Flask

--- finndex/__init__.py
+++ finndex/__init__.py
@@ -2,9 +2,9 @@
 Sets up the library by starting the NLP sever and initializing the graphing engine's date format.
 '''
 
+from finndex.api import datadict
+from finndex.sentiment import nlp
 from pandas.plotting import register_matplotlib_converters
-
-import finndex.sentiment.nlp as nlp
 
 __author__ = "Finn Frankis"
 __copyright__ = "Copyright 2019, Crypticko"
@@ -12,3 +12,5 @@ __copyright__ = "Copyright 2019, Crypticko"
 
 nlp.startServer(nlp.STANFORD_NLP_TIMEOUT, nlp.STANFORD_NLP_PORT)
 register_matplotlib_converters() # register MatPlotLib date converter
+
+a = datadict.setupApi('__main__')
