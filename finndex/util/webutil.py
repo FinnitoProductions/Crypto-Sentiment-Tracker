58ccb71be6e435383fd2eb71d52253e531db15f5
Finn Frankis <finn@teachmy.com>
2020-06-17 20:18:16 -0700

    Add article retrieval for CNN, B.I., and CoinDesk

--- finndex/util/webutil.py
+++ finndex/util/webutil.py
@@ -24,14 +24,48 @@ def getPageContent(url):
 def parseHTML(url):
     return BeautifulSoup(getPageContent(url), 'html.parser')
 
-# Extracts the text from a CNN article with given URL, excluding the headline and any advertisements.
-def getCNNText(url):
-    htmlParser = parseHTML(url)
-    
-    text = ''
+def get_cnn_text(url):
+   '''
+   ' Retrieves the body of a given CNN article, excluding the headline and any advertisements.
+   '
+   ' url (string): the URL where the specific article is located
+   '''
+   htmlParser = parseHTML(url)
+   
+   text = ''
 
-    for element in htmlParser.select('div'):
-        if element.has_attr('class') and 'zn-body__paragraph' in element['class']: 
-            text += element.text 
-    text = text.replace('"', ' ')
-    return text
+   for element in htmlParser.select('div'):
+      if element.has_attr('class') and 'zn-body__paragraph' in element['class']: 
+         text += element.text 
+   text = text.replace('"', ' ')
+   return text
+
+def get_coin_desk_text(url):
+   '''
+   ' Retrieves the body of a given CoinDesk article.
+   '
+   ' url (string): the URL where the specific article is located
+   '''
+   parser = parseHTML(url)
+
+   text = ""
+   for element in parser.findAll("div", {"class": "article-pharagraph"}):
+      text += element.text
+      
+   return text
+
+def get_business_insider_text(url):
+   '''
+   ' Retrieves the body of a given Business Insider article.
+   '
+   ' url (string): the URL where the specific article is located
+   '''
+   parser = parseHTML(url)
+   text = ""
+   paragraphs = parser.findAll("p", {"class": ""})
+
+   for element in paragraphs:
+      if element.img == None:
+         text += element.text
+
+   return text.replace("\n", "").replace("\xa0", " ")
