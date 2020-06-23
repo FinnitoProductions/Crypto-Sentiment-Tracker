5a9b7d3406112b843fd5b738ea0af156569aabc1
Finn Frankis <finn@teachmy.com>
2020-06-23 16:30:28 -0700

    Update initialization to use plotly backend

--- finndex/__init__.py
+++ finndex/__init__.py
@@ -4,6 +4,7 @@ Sets up the library by starting the NLP sever and initializing the graphing engi
 
 from finndex.api import datadict
 from finndex.sentiment import nlp
+import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 
 __author__ = "Finn Frankis"
@@ -12,5 +13,6 @@ __copyright__ = "Copyright 2019, Crypticko"
 
 nlp.startServer(nlp.STANFORD_NLP_TIMEOUT, nlp.STANFORD_NLP_PORT)
 register_matplotlib_converters() # register MatPlotLib date converter
+pd.options.plotting.backend = "plotly"
 
 a = datadict.setupApi('__main__')
